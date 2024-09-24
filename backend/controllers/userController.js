22de39ef5f11bd8499af4851fab605a2bdf61a28
Sachin Gyandor <gyandors@gmail.com>
2024-09-24 23:10:45 +0530

    Added encryption to the password
    Using bcrypt package
    
    When creating a new user the bcrypt.hash() method is used for encryption
    
    when user login the bcrypt.compare() method is used

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcrypt");
+
 const User = require("../models/userModel");
 
 /**
@@ -36,7 +38,14 @@ exports.postUserSignup = async (req, res, next) => {
       throw error;
     }
 
-    const user = await User.create({ firstName, lastName, email, password });
+    const encryptedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      firstName,
+      lastName,
+      email,
+      password: encryptedPassword,
+    });
 
     res.status(201).json(user);
   } catch (error) {
@@ -55,10 +64,21 @@ exports.postUserSignin = async (req, res, next) => {
   try {
     const { email, password } = req.body;
 
+    if (email.trim().length < 1 || password.trim().length < 1) {
+      const error = new Error("Enter the required details.");
+      error.code = 400;
+      throw error;
+    }
+
     const existingUser = await User.findAll({ where: { email: email } });
 
     if (existingUser[0]) {
-      if (existingUser[0].dataValues.password === password) {
+      const encryptedPassword = existingUser[0].dataValues.password;
+      const decryptedPassword = await bcrypt.compare(
+        password,
+        encryptedPassword
+      );
+      if (decryptedPassword) {
         res.status(200).json(existingUser[0]);
       } else {
         const error = new Error("User not authorized.");
